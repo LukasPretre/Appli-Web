23b739d15001bc8b99782d0bfe641ce8a23ebc4a
Lukas PRÊTRE <lukpretre@icloud.com>
2025-03-11 18:07:53 +0100

    Ajout initial du projet

---
+++ pizza_mama/Models/Pizza.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text.Json.Serialization;
+
+namespace pizza_mama.Models
+{
+    public class Pizza
+    {
+        [JsonIgnore]
+        public int PizzaID { get; set; }
+
+        [Display(Name = "Nom")]
+        public string nom { get; set; }
+
+        [Display(Name = "Prix (€)")]
+        public float prix { get; set; }
+
+        [Display(Name = "Végétarienne")]
+        public bool vegetarienne { get; set; }
+
+        [Display(Name = "Ingrédient")]
+        [JsonIgnore]
+        public string ingredient { get; set; }
+
+        [NotMapped]
+        [JsonPropertyName("ingredient")]
+        public string[] listeIngredients
+        {
+            get
+            {
+                if((ingredient == null) ||(ingredient.Count() == 0))
+                {
+                    return null;
+                }
+                return ingredient.Split(", ");
+
+            }
+        }
+    }
+}
